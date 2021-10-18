32f9576ce0357318a8e8013439b232683298f8ee
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2021-10-18 18:08:28 -0500

    Viewport changes in mobile

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const menuLinks = document.querySelector('.nav-menu')
 
-/* When menu is clicked, toggle active-ness of object */
+// Toggle when menu is clicked
 menu.addEventListener('click', function() {
-    /* Toggles and adds a classList to 'menu' */
+    //Adds a classlist to menu
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
