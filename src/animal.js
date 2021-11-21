4182f8e32f5627551e3fcf2209dc1a3f81b1b380
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2021-11-21 16:05:03 -0600

    Filter changes

--- src/animal.js
+++ src/animal.js
@@ -1,11 +1,12 @@
 /*
     TODO: 
+    - Mobile view
+        - hamburger menu setup
+        - filter system
     - Implement "Search by breed"
-    - Random images on page load
     - Loading animation while waiting for API
-    - Change "About me" html layout
+    - Random name assignment
 */
-
 
 const imageRandom = document.querySelector(".pet-img");
 const petSpace = document.querySelector(".pet-space")
@@ -14,10 +15,9 @@ const sideBarList = document.querySelector(".pet-selection")
 const randomImageApiUrl = "https://dog.ceo/api/breeds/image/random";
 const sidebarInput = document.getElementById("sidebar-input");
 
-// filter sidebar
-document.addEventListener('keyup', () => {
-    console.log('Key was pressed');
-})
+let mainSectionSize = document.querySelector(".main-container");
+let sidebarSize = document.querySelector(".sidebar");
+
 
 // Dynamic html selection 
 function getDogBreedImages( breedName) {
@@ -59,9 +59,36 @@ sideBarList.addEventListener('click', event => {
         getDogBreedImages(event.target.innerHTML.trim());
     }
 })
-// END OF DYNAMIC SECTION
 
-// Side bar breed list
+// Filter Breed List
+function filterBreedList() {
+    let breedList = document.querySelector(".pet-selection");
+    let listOfBreeds = breedList.getElementsByTagName("LI");
+    let userInput = document.getElementById("sidebar-input");
+    
+    for( let i = 0; i < listOfBreeds.length ; i++ ) {
+        let breed = listOfBreeds[i].getElementsByTagName("LI")
+
+        if (userInput.value[i] != listOfBreeds[i].innerHTML[0]) {
+            listOfBreeds[i].style.display = "none";
+        }
+       //console.log("User: " + userInput.value[0] + " Breed " + listOfBreeds[i].innerHTML[0]);
+    }
+}
+
+// END
+
+
+// SIDEBAR CREATION
+function createSidebar(breedList) {
+    sideBarList.innerHTML = '';
+
+    breedList.forEach( breed => {
+        let sideBarHTML = `<li class="sidebar-li">${breed}</li>`;
+        sideBarList.innerHTML += sideBarHTML;
+    })
+}
+
 function createDogBreedList() {
     const allBreedsApiUrl = "https://dog.ceo/api/breeds/list/all";
 
@@ -70,20 +97,28 @@ function createDogBreedList() {
         return response.json();
     })
     .then( (json) => {
-        let allBreeds = json.message;
-        let breedList = Object.keys(allBreeds);
-
-        sideBarList.innerHTML = '';
-
-        breedList.forEach( breed => {
-            let sideBarHTML = `<li class="sidebar-li">${breed}</li>`;
-            sideBarList.innerHTML += sideBarHTML;
-        })
+        createSidebar(Object.keys(json.message) );
     })
     .catch( (error) => {
         console.log(error);
     });
 }
+
+// Resizing grid
+visualViewport.addEventListener('resize', () => {
+    
+    let sizeDif = mainSectionSize.offsetWidth - sideBarList.offsetWidth;
+    
+    if ( sizeDif > 990) {
+        document.querySelector(".pet-space").style["grid-template-columns"] = "auto auto auto";
+    }
+    else if ( sizeDif > 570 && sizeDif < 990) {
+        document.querySelector(".pet-space").style["grid-template-columns"] = "auto auto";
+    }
+    else if ( sizeDif < 570) {
+        document.querySelector(".pet-space").style["grid-template-columns"] = "auto";
+    }
+})
 
 // INIT page settings
 function createRandomPet() {
@@ -101,7 +136,7 @@ function createRandomPet() {
 
 function createAge() {
     let years = Math.round( Math.random() * 12);
-    let ageStatement = years + " years"; 
+    let ageStatement = years + " years old"; 
     return ageStatement;
 }
 
@@ -136,7 +171,6 @@ function renderAdoptPage() {
     for(let i =0; i< 30; i++) {
         createRandomPet();
     }
-
     // Promise.all( imgArray.map( () => {
     //     fetch(randomImageApiUrl)
     //     .then( json => {
