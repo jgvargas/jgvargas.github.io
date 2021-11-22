32568618d52d78f426668a5bc0184f8e75902a44
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2021-11-22 11:06:06 -0600

    Filter working with random name assignment

--- src/animal.js
+++ src/animal.js
@@ -3,10 +3,12 @@
     - Mobile view
         - hamburger menu setup
         - filter system
-    - Implement "Search by breed"
     - Loading animation while waiting for API
     - Random name assignment
+        - Some assigned Undefined
+        - Age should not be 0
 */
+
 
 const imageRandom = document.querySelector(".pet-img");
 const petSpace = document.querySelector(".pet-space")
@@ -36,18 +38,24 @@ function getDogBreedImages( breedName) {
 
 function createSpecificBreedHtml(json, breedName) {
     let imageList = json.message;
-    let name = getName();
-    let gender = Math.round (Math.random() );
-    let age = createAge();
+    let name;
+    
 
     petSpace.innerHTML ="";
 
     imageList.forEach( imageSrc => {
+        let gender = ( Math.round (Math.random() ) ? "Female" : "Male"  );
+        if (gender == "Male")
+            name = getName(true);
+        else 
+            name = getName(false);
+        let age = createAge();
+
         petSpace.innerHTML += 
         `<div class="pet-box">
             <img src="${imageSrc}" class="pet-img" alt="friendly-dog">
-            <div class="pet-info"><h3>${name}</h3>
-            <p>${ gender ? "Female" : "Male" } | ${age}</p> 
+            <div class="pet-info"><h3>${name} | ${ gender }</h3>
+            <p> ${age} ${capitalizeFirstLetter( breedName )}</p> 
             </div>
         </div>`;
     })
@@ -65,14 +73,17 @@ function filterBreedList() {
     let breedList = document.querySelector(".pet-selection");
     let listOfBreeds = breedList.getElementsByTagName("LI");
     let userInput = document.getElementById("sidebar-input");
-    
-    for( let i = 0; i < listOfBreeds.length ; i++ ) {
-        let breed = listOfBreeds[i].getElementsByTagName("LI")
+    let Input = userInput.value.toLowerCase();
 
-        if (userInput.value[i] != listOfBreeds[i].innerHTML[0]) {
+    for( let i = 0; i < listOfBreeds.length ; i++ ) {
+        let breed = listOfBreeds[i].innerHTML;
+
+        if ( breed.toLowerCase().indexOf(Input) > -1 ) {
+            listOfBreeds[i].style.display = "";
+        }
+        else {
             listOfBreeds[i].style.display = "none";
         }
-       //console.log("User: " + userInput.value[0] + " Breed " + listOfBreeds[i].innerHTML[0]);
     }
 }
 
@@ -136,28 +147,260 @@ function createRandomPet() {
 
 function createAge() {
     let years = Math.round( Math.random() * 12);
-    let ageStatement = years + " years old"; 
+    let ageStatement = years + " year old"; 
     return ageStatement;
 }
 
-function getName() {
-    let nameFile = new XMLHttpRequest();
-    nameFile.open("GET", "pet-names.txt", true);
+function getName(gender) {
+    let female = [
+        "Bella",
+        "Lucy",
+        "Daisy",
+        "Molly",
+        "Lola",
+        "Sophie",
+        "Sadie",
+        "Maggie",
+        "Chloe",
+        "Bailey",
+        "Roxy",
+        "Zoey",
+        "Lily",
+        "Luna",
+        "Coco",
+        "Stella",
+        "Gracie",
+        "Abby",
+        "Penny",
+        "Zoe",
+        "Ginger",
+        "Ruby",
+        "Rosie",
+        "Lilly",
+        "Ellie",
+        "Mia",
+        "Sasha",
+        "Lulu",
+        "Pepper",
+        "Nala",
+        "Lexi",
+        "Lady",
+        "Emma",
+        "Riley",
+        "Dixie",
+        "Annie",
+        "Maddie",
+        "Piper",
+        "Princess",
+        "Izzy",
+        "Maya",
+        "Olive",
+        "Cookie",
+        "Roxie",
+        "Angel",
+        "Belle",
+        "Layla",
+        "Missy",
+        "Cali",
+        "Honey",
+        "Millie",
+        "Harley",
+        "Marley",
+        "Holly",
+        "Kona",
+        "Shelby",
+        "Jasmine",
+        "Ella",
+        "Charlie",
+        "Minnie",
+        "Willow",
+        "Phoebe",
+        "Callie",
+        "Scout",
+        "Katie",
+        "Dakota",
+        "Sugar",
+        "Sandy",
+        "Josie",
+        "Macy",
+        "Trixie",
+        "Winnie",
+        "Peanut",
+        "Mimi",
+        "Hazel",
+        "Mocha",
+        "Cleo",
+        "Hannah",
+        "Athena",
+        "Lacey",
+        "Sassy",
+        "Lucky",
+        "Bonnie",
+        "Allie",
+        "Brandy",
+        "Sydney",
+        "Casey",
+        "Gigi",
+        "Baby",
+        "Madison",
+        "Heidi",
+        "Sally",
+        "Shadow",
+        "Cocoa",
+        "Pebbles",
+        "Misty",
+        "Nikki",
+        "Lexie",
+        "Grace",
+        "Sierra"
+    ];
 
-    return "Bob"
+    let male = [
+        "Max",
+        "Buddy",
+        "Charlie",
+        "Jack",
+        "Cooper",
+        "Rocky",
+        "Toby",
+        "Tucker",
+        "Jake",
+        "Bear",
+        "Duke",
+        "Teddy",
+        "Oliver",
+        "Riley",
+        "Bailey",
+        "Bentley",
+        "Milo",
+        "Buster",
+        "Cody",
+        "Dexter",
+        "Winston",
+        "Murphy",
+        "Leo",
+        "Lucky",
+        "Oscar",
+        "Louie",
+        "Zeus",
+        "Henry",
+        "Sam",
+        "Harley",
+        "Baxter",
+        "Gus",
+        "Sammy",
+        "Jackson",
+        "Bruno",
+        "Diesel",
+        "Jax",
+        "Gizmo",
+        "Bandit",
+        "Rusty",
+        "Marley",
+        "Jasper",
+        "Brody",
+        "Roscoe",
+        "Hank",
+        "Otis",
+        "Bo",
+        "Joey",
+        "Beau",
+        "Ollie",
+        "Tank",
+        "Shadow",
+        "Peanut",
+        "Hunter",
+        "Scout",
+        "Blue",
+        "Rocco",
+        "Simba",
+        "Tyson",
+        "Ziggy",
+        "Boomer",
+        "Romeo",
+        "Apollo",
+        "Ace",
+        "Luke",
+        "Rex",
+        "Finn",
+        "Chance",
+        "Rudy",
+        "Loki",
+        "Moose",
+        "George",
+        "Samson",
+        "Coco",
+        "Benny",
+        "Thor",
+        "Rufus",
+        "Prince",
+        "Chester",
+        "Brutus",
+        "Scooter",
+        "Chico",
+        "Spike",
+        "Gunner",
+        "Sparky",
+        "Mickey",
+        "Kobe",
+        "Chase",
+        "Oreo",
+        "Frankie",
+        "Mac",
+        "Benji",
+        "Bubba",
+        "Champ",
+        "Brady",
+        "Elvis",
+        "Copper",
+        "Cash",
+        "Archie",
+        "Walter"
+    ];
+
+    if(gender){
+        return male[ Math.floor( Math.random() * (male.length - 1 + 1) + 1) ]; 
+    }
+    else {
+        return female[ Math.floor( Math.random() * (male.length - 1 + 1) + 1) ]; 
+    }
+}
+
+function capitalizeFirstLetter(string) {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+function getBreedName(imgURL) {
+    let urlSplit = imgURL.split("/");
+    let breedName = "";
+
+    if (urlSplit[4].indexOf("-") != -1) {
+        let multiWord = urlSplit[4].split("-");
+        breedName = capitalizeFirstLetter( multiWord[1] ) + " " + capitalizeFirstLetter( multiWord[0] );
+        return breedName;
+    }
+    else {
+        breedName = urlSplit[4]
+        return capitalizeFirstLetter( breedName );
+    }
 }
 
 function createPetSpace (json) {
     let randImg = json.message;
-    let name = getName();
-    let gender = Math.round (Math.random() );
+    let name;
+    let gender = ( Math.round (Math.random() ) ? "Female" : "Male"  );
+    if (gender == "Male")
+        name = getName(true);
+    else 
+        name = getName(false);
     let age = createAge();
+    let breed = getBreedName(randImg);
 
     petSpace.innerHTML += 
     `<div class="pet-box">
         <img src="${randImg}" class="pet-img" alt="friendly-dog">
-        <div class="pet-info"><h3>${name}</h3>
-        <p>${ gender ? "Female" : "Male" } | ${age}</p> 
+        <div class="pet-info"><h3>${name} | ${ gender }</h3>
+        <p>${age} ${breed}</p> 
         </div>
     </div>`;
 }
