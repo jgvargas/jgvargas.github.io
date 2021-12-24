8216daa206d539b32237a98520989b334b71aa78
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2021-12-24 09:13:55 -0600

    Added theme toggle

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.nav-menu')
+const toggle = document.querySelector('.toggle')
 
 // Toggle when menu is clicked
-menu.addEventListener('click', function() {
+menu.addEventListener('click', () => {
     //Adds a classlist to menu
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+// theme change
+toggle.addEventListener('change', (event) => {
+    const isChecked = event.target.checked;
+
+    if(isChecked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+})
