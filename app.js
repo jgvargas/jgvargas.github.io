0bebd9bc4780cd34788a082d204008d1f4a55614
Juan G. Vargas Jr <18316297+jgvargas@users.noreply.github.com>
2022-03-25 15:34:11 -0500

    New layout

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.nav-menu')
-const toggle = document.querySelector('.toggle')
-let storedTheme = localStorage.getItem("userTheme")
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
 
-// Theme logic
-function loadTheme() {
-    console.log("Hello")
-}
 
-if(storedTheme) {
-    console.log(`The stored theme is ${storedTheme}`)
-    document.documentElement.setAttribute('data-theme', storedTheme)
-    if(storedTheme === 'dark') {
-        toggle.classList.toggle('night')
-    }
-}
 
-toggle.addEventListener('change', (event) => {
-    toggle.classList.toggle('night')
-    let theme = 'light'
-    storedTheme = document.documentElement.getAttribute('data-theme')
-    
-    if(storedTheme === "light" || storedTheme === null) {
-        theme = 'dark'
-    }
-    localStorage.setItem('userTheme', theme)
-    document.documentElement.setAttribute('data-theme', theme)
-})
-
-// Mobile menu
-menu.addEventListener('click', () => {
+// Toggle when menu is clicked
+menu.addEventListener('click', function() {
+    //Adds a classlist to menu
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
 
